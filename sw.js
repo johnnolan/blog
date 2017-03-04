a8c1cd084614306a0a5c3704740fd4ec84927f43
John Nolan <johnnolan@nolanscafe.co.uk>
2017-03-04 16:02:10 +0000

    Minor fixes for AMP Stuff. Analytics is a pain

--- sw.js
+++ sw.js
@@ -14,12 +14,11 @@ self.addEventListener('install', e => {
 			'{{ post.url }}',
 			{% endfor %}
 
-			{% for page in site.html_pages %}
-			'{{ page.url | prepend: site.canonical_baseurl }}',
-			{% endfor %}
 			{% for post in site.posts %}
 			'{{ post.url | prepend: site.canonical_baseurl }}',
 			{% endfor %}
+
+			'/amp/index.html'
 		])
 			.then(() => self.skipWaiting());
 })
